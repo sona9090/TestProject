ee1813b9ad96adf62b4f2c10514db1c6eb0e48c1
f <f>
2022-06-04 15:21:29 +0400

    With_redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
 import React, { ChangeEventHandler, useEffect, useState } from "react";
+import { createStore } from "redux";
 import "./App.css";
 import { CatsDataType, CategoriesType } from "./types/Types";
 
 const App = () => {
   const [category, setCategory] = useState("");
-  const [page, setPage] = useState("1");
+  // const [page, setPage] = useState("1");
   const [catsData, setCatsData] = useState([]);
 
   const [categs, setCategs] = useState([]);
 
-  const url = `https://api.thecatapi.com/v1/images/search?limit=10&page=${page}&category_ids=${category}`;
+  const initialState = {
+    page: "1"
+  }
+
+  const reducer = (state = initialState, action: {type: string}) => {
+    switch (action.type) {
+      case 'page/first':
+        return { page: "1" }
+      case 'page/next':
+        return { page: (parseInt(state.page) + 1).toString() }
+      case 'page/prev':
+        return { page: (parseInt(state.page) - 1).toString() }
+    }
+  
+    return state
+  }
+
+  const store = createStore(reducer);
+
   const categories = "https://api.thecatapi.com/v1/categories";
+
+  const [url, setUrl] = useState(`https://api.thecatapi.com/v1/images/search?limit=10&page=1&category_ids=${category}`)
+
+  store.subscribe(() => setUrl(`https://api.thecatapi.com/v1/images/search?limit=10&page=${store.getState().page}&category_ids=${category}`))
 
   useEffect(() => {
     const fetchData = async () => {
@@ -33,14 +56,14 @@ const App = () => {
     fetchData();
   }, [url]);
 
-  const noPrev = page === "1";
+  const noPrev = store.getState().page === "1";
 
   return (
     <>
       <select
         id="category"
         onChange={(e) => {
-          setPage("1");
+          store.dispatch({ type: 'page/first' });
           setCategory(e.currentTarget.value);
         }}
       >
@@ -69,7 +92,7 @@ const App = () => {
           id="prev"
           disabled={noPrev}
           onClick={() => {
-            setPage((parseInt(page) - 1).toString());
+            store.dispatch({ type: 'page/prev' });
           }}
         >
           Prev
@@ -77,7 +100,8 @@ const App = () => {
         <button
           id="next"
           onClick={() => {
-            setPage((parseInt(page) + 1).toString());
+            // setPage((parseInt(page) + 1).toString());
+            store.dispatch({ type: 'page/next' });
           }}
         >
           Next
